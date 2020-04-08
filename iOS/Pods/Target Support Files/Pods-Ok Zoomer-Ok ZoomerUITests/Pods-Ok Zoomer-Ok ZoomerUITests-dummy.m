bc2f1c332768caf256b2b3eaa355ff170ff913b3
Max Gilespie <max.gillespie10@gmail.com>
2020-04-08 16:26:49 -0400

    refactor + add pod

---
+++ iOS/Pods/Target Support Files/Pods-Ok Zoomer-Ok ZoomerUITests/Pods-Ok Zoomer-Ok ZoomerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ok_Zoomer_Ok_ZoomerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Ok_Zoomer_Ok_ZoomerUITests
+@end
---
+++ iOS/Pods/Target Support Files/Pods-Ok ZoomerTests/Pods-Ok ZoomerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ok_ZoomerTests : NSObject
+@end
+@implementation PodsDummy_Pods_Ok_ZoomerTests
+@end
